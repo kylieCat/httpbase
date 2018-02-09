f3a52ba7be77f9d97ec60e9c6f181fff297da344
Ian Auld <imauld@gmail.com>
2018-02-08 16:50:09 -0800

    Add py version check, expose some stuff via init  # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # Date: Thu Feb 8 16:50:09 2018 -0800 # # On branch master # Changes to be committed: # modified: httpbase/__init__.py # modified: requirements.txt # modified: setup.py # []

--- httpbase/__init__.py
+++ httpbase/__init__.py
@@ -1,4 +1,9 @@
+import requests
+
 from .client import HTTPBaseClient
 from .constants import HTTPMethods, HTTPResponseCodes
 from .resources import Resource
 from .routes import Route
+
+# Alias the Response class form requests so it can be used as a type hint with having to import form a dependency
+Response = requests.Response
--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import find_packages, setup
+import sys
 
+
+if sys.version_info < (3, 6):
+    sys.exit('Sorry, Python < 3.6 is not supported')
 
 setup(
     name='httpbase',
-    version="0.1.0",
+    version="0.1.1",
     author='Ian Auld',
     author_email='imauld@gmail.com',
     description="Library for quickly making those simple HTTP clients we all end up writing all the time",

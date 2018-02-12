de5e63eb2ee826ec5deb81a70e7763a9db256f15
Ian Auld <imauld@gmail.com>
2018-02-12 09:59:21 -0800

    Update example to use new code []

--- example.py
+++ example.py
@@ -1,21 +1,30 @@
-from datetime import datetime
 import sys
 from typing import NamedTuple
 
-from httpbase import Route, HTTPBaseClient, Resource, HTTPResponseCodes, HTTPMethods, Response
+from httpbase import Response
+from httpbase.client import HTTPBaseClient
+from httpbase.constants import HTTPResponseCodes, HTTPMethods
+from httpbase.fields import IntField, StrField
+from httpbase.resources import Resource
+from httpbase.routes import Route
+
+
+def title_max_length(value):
+    if len(value) > 50:
+        raise TypeError("title too long")
+    return str(value)
 
 
 class Post(Resource):
-    # Sometimes the Pythonic naming conventions won't match with the service you are calling. This can help.
-    _mapped_fields = [("user_id", "userId")]
-
-    def __init__(self, title: str, body: str, user_id: int, id: int=None, **kwargs):
-        # Don't forget to call super!
-        super().__init__()
-        self.id = id
-        self.user_id = user_id
-        self.title = title
-        self.body = body
+    # This field can be null.
+    id = IntField(label="id", nullable=True)
+    # Sometimes the Pythonic naming conventions won't match with the service you are calling. `label` can help.
+    # Set a default value for all instances of this resource
+    user_id = IntField(label="userId", default=123)
+    # add a callable validator to make sure values conform to any rules the API may have. The callable should take one
+    # parameter, value, and return a the value if it's valid or raise a ``TypeError``.
+    title = StrField(label="title", validator=title_max_length)
+    body = StrField(label="body")
 
 
 # You don't have to do this but I prefer to have all the routes in a container
@@ -47,7 +56,7 @@ class PostClient(HTTPBaseClient):
     def get_post(self, post_id) -> Response:
         return self._make_request(routes.get_post, post_id=post_id)
 
-    def search_posts(self, **params):
+    def search_posts(self, **params) -> Response:
         return self._make_request(routes.search_posts, params=params)
 
 
@@ -56,8 +65,8 @@ if __name__ == "__main__":
     client = PostClient("https://jsonplaceholder.typicode.com")
 
     # Create a resource
-    todo = Post("My first Post", "Some body text", 1)
-    response = client.new_post(todo)
+    post = Post(title="My first Post", body="Some body text", user_id=1)
+    response = client.new_post(post)
     print(response.json())
 
     # Use constants like a civilized person

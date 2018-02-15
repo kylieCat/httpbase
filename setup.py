a6c9349600c0a00eee400b4d868bf6639ff97166
Ian Auld <imauld@gmail.com>
2018-02-15 14:38:44 -0800

    Increment version []

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (3, 6):
 
 setup(
     name='httpbase',
-    version="0.4.4",
+    version="0.5.0",
     author='Ian Auld',
     author_email='imauld@gmail.com',
     description="Library for quickly making those simple HTTP clients we all end up writing all the time",
